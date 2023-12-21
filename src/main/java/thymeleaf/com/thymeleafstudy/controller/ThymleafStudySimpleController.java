207247cb88c305a0564581c1e829b742efad957b
HosseinJavooni <h.javooni@gmail.com>
2023-12-21 16:34:36 +0330

    Thu Dec 21 04:34:36 PM +0330 2023

--- src/main/java/thymeleaf/com/thymeleafstudy/controller/ThymleafStudySimpleController.java
+++ src/main/java/thymeleaf/com/thymeleafstudy/controller/ThymleafStudySimpleController.java
@@ -1,8 +1,5 @@
 package thymeleaf.com.thymeleafstudy.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +10,28 @@ import thymeleaf.com.thymeleafstudy.model.Person;
 @Controller
 public class ThymleafStudySimpleController{
     @GetMapping
+    public String homePage(){
+        return "homePage";
+    }
+    @GetMapping("/person-thymeleaf-variable-expression")
     public String returnModel(Model model){
-        var persons = new ArrayList<Person>();
         var address = new Address("zarrin ghobaie", "123");
-        persons.add(new Person(1, "Hossein", "Jabani", address));
-
         model.addAttribute("person", new Person(2, "Mahan", "Jabani", address));
+        return "person_thymeleafVariableExpression";
+    }
+    @GetMapping("/simpleAttribute-thymeleaf-simple-expression")
+    public String returnSimpleAttribute(Model model){
         model.addAttribute("Hi", "Hi Thymeleaf World!");
-        return "index";
+        return "simpleAttribute_thymeleafSimpleExpression";
+    }
+    @GetMapping("/person-thymeleaf-object-expression")
+    public String returnPersonObject(Model model){
+        var address = new Address("zarrin ghobaie", "123");
+        model.addAttribute("person", new Person(2, "Mahan", "Jabani", address));
+        return "person_thymeleafObjectExpression";
+    }
+    @GetMapping("/messages-thymeleaf-message-expression")
+    public String returnMessages(){
+        return "messages_thymeleafMessageExpression";
     }
 }
